e16467c7b55589bc7161319a160f45b245d3a623
Jemandeep <141459701+Jemandeep@users.noreply.github.com>
2023-09-15 22:58:25 -0600

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
@@ -8,45 +8,45 @@ class Program
     static void Main()
     {
         // Task 1: Creating Variables
-        int lowNumber, highNumber;
+        int LowNumber, HighNumber;
 
         // Prompt the user to enter the low number
         Console.Write("Enter the low number: ");
-        lowNumber = int.Parse(Console.ReadLine());
+        LowNumber = int.Parse(Console.ReadLine());
 
         // Prompt the user to enter the high number
         Console.Write("Enter the high number: ");
-        highNumber = int.Parse(Console.ReadLine());
+        HighNumber = int.Parse(Console.ReadLine());
 
         // Calculate and print the difference
-        int difference = highNumber - lowNumber;
-        Console.WriteLine($"The difference between {highNumber} and {lowNumber} is: {difference}");
+        int difference = HighNumber - LowNumber;
+        Console.WriteLine($"The difference between {HighNumber} and {LowNumber} is: {difference}");
 
         // Task 2: Looping and Input Validation
         // Loop to ensure a positive low number
         while (true)
         {
             Console.Write("Enter a positive low number: ");
-            lowNumber = int.Parse(Console.ReadLine());
-            if (lowNumber > 0)
+            LowNumber = int.Parse(Console.ReadLine());
+            if (LowNumber > 0)
                 break;
         }
 
         // Loop to ensure a high number greater than lowNumber
         while (true)
         {
-            Console.Write($"Enter a high number greater than {lowNumber}: ");
-            highNumber = int.Parse(Console.ReadLine());
-            if (highNumber > lowNumber)
+            Console.Write($"Enter a high number greater than {LowNumber}: ");
+            HighNumber = int.Parse(Console.ReadLine());
+            if (HighNumber > LowNumber)
                 break;
         }
 
         // Task 3: Using Arrays and File I/O
-        int[] numbersArray = new int[highNumber - lowNumber + 1];
+        int[] numbersArray = new int[HighNumber - LowNumber + 1];
 
         for (int i = 0; i < numbersArray.Length; i++)
         {
-            numbersArray[i] = lowNumber + i;
+            numbersArray[i] = LowNumber + i;
         }
 
         // Write numbers to the file in reverse order
